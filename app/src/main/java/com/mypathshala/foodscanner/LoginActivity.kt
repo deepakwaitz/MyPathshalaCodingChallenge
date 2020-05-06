debc20be650ca82cfbf3cb08d808f2cf4ba137c7
Deepak <deepakwaitz@gmail.com>
2020-05-06 09:04:57 +0530

    - Sign in and signout are working

---
+++ app/src/main/java/com/mypathshala/foodscanner/LoginActivity.kt
@@ -0,0 +1,105 @@
+package com.mypathshala.foodscanner
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    companion object {
+        private const val RC_SIGN_IN = 123
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        createSignIntent()
+    }
+
+    private fun createSignIntent() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.TwitterBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                finish()
+                // ...
+            } else {
+                Log.d("Sign in failed - ", " " + response?.error?.message)
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+
+    private fun delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_delete]
+    }
+
+    /* private fun themeAndLogo() {
+         val providers = emptyList<AuthUI.IdpConfig>()
+
+         // [START auth_fui_theme_logo]
+         startActivityForResult(
+                 AuthUI.getInstance()
+                         .createSignInIntentBuilder()
+                         .setAvailableProviders(providers)
+                         .setLogo(R.drawable.my_great_logo) // Set logo drawable
+                         .setTheme(R.style.MySuperAppTheme) // Set theme
+                         .build(),
+                 RC_SIGN_IN)
+         // [END auth_fui_theme_logo]
+     }*/
+
+    private fun privacyAndTerms() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+        // [START auth_fui_pp_tos]
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                    "https://example.com/terms.html",
+                    "https://example.com/privacy.html"
+                )
+                .build(),
+            RC_SIGN_IN
+        )
+        // [END auth_fui_pp_tos]
+    }
+}
